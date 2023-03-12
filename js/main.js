f11712ec2e297ef71df2a9253c2b52a86d03c943
AmoghPadukone <35802992+AmoghPadukone@users.noreply.github.com>
2023-03-12 11:04:59 +0530

    responsive about us section

--- js/main.js
+++ js/main.js
@@ -1,69 +1,71 @@
-$(window).load(function(){
-     $('.preloader').fadeOut('slow');
+$(window).load(function () {
+  $(".preloader").fadeOut("slow");
 });
-
 
 /* =Main INIT Function
 -------------------------------------------------------------- */
 function initializeSite() {
+  "use strict";
 
-	"use strict";
+  //OUTLINE DIMENSION AND CENTER
+  (function () {
+    function centerInit() {
+      var sphereContent = $(".sphere"),
+        sphereHeight = sphereContent.height(),
+        parentHeight = $(window).height(),
+        topMargin = (parentHeight - sphereHeight) / 2;
 
-	//OUTLINE DIMENSION AND CENTER
-	(function() {
-	    function centerInit(){
+      sphereContent.css({
+        "margin-top": topMargin + "px",
+      });
 
-			var sphereContent = $('.sphere'),
-				sphereHeight = sphereContent.height(),
-				parentHeight = $(window).height(),
-				topMargin = (parentHeight - sphereHeight) / 2;
+      var heroContent = $(".hero"),
+        heroHeight = heroContent.height(),
+        heroTopMargin = (parentHeight - heroHeight) / 2;
 
-			sphereContent.css({
-				"margin-top" : topMargin+"px"
-			});
+      heroContent.css({
+        "margin-top": heroTopMargin + "px",
+      });
+    }
 
-			var heroContent = $('.hero'),
-				heroHeight = heroContent.height(),
-				heroTopMargin = (parentHeight - heroHeight) / 2;
+    $(document).ready(centerInit);
+    $(window).resize(centerInit);
+  })();
 
-			heroContent.css({
-				"margin-top" : heroTopMargin+"px"
-			});
-
-	    }
-
-	    $(document).ready(centerInit);
-		$(window).resize(centerInit);
-	})();
-
-	// Init effect 
-	$('#scene').parallax();
-
-};
+  // Init effect
+  $("#scene").parallax();
+}
 /* END ------------------------------------------------------- */
 
 /* =Document Ready Trigger
 -------------------------------------------------------------- */
-$(window).load(function(){
-
-	initializeSite();
-	(function() {
-		setTimeout(function(){window.scrollTo(0,0);},0);
-	})();
-
+$(window).load(function () {
+  initializeSite();
+  (function () {
+    setTimeout(function () {
+      window.scrollTo(0, 0);
+    }, 0);
+  })();
 });
 /* END ------------------------------------------------------- */
 
-
-$('#countdown').countdown({
-	date: "December 14, 2019 18:03:26",
-	render: function(data) {
-	  var el = $(this.el);
-	  el.empty()
-	    //.append("<div>" + this.leadingZeros(data.years, 4) + "<span>years</span></div>")
-	    .append("<div>" + this.leadingZeros(data.days, 2) + " <span>days</span></div>")
-	    .append("<div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div>")
-	    .append("<div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div>")
-	    .append("<div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
-	}
+$("#countdown").countdown({
+  date: "December 14, 2019 18:03:26",
+  render: function (data) {
+    var el = $(this.el);
+    el.empty()
+      //.append("<div>" + this.leadingZeros(data.years, 4) + "<span>years</span></div>")
+      .append(
+        "<div>" + this.leadingZeros(data.days, 2) + " <span>days</span></div>"
+      )
+      .append(
+        "<div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div>"
+      )
+      .append(
+        "<div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div>"
+      )
+      .append(
+        "<div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>"
+      );
+  },
 });
