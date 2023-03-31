c0fe3ab42d6bb62917821af68c757204520916d1
charana555 <charanchandrashekar55@gamil.com>
2023-03-31 15:06:04 +0530

    added json data and modified try.html and fresh.js

---
+++ eventData.js
@@ -0,0 +1,218 @@
+export const eventData4Star = [
+  {
+    title: "ClASSICAL SOLO DANCE",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["DANCE", "5000", "3000"],
+  },
+  {
+    title: "WESTERN STREET SOLO",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["DANCE", "5000", "3000"],
+  },
+  {
+    title: "INDIAN CLASSICAL SOLO",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["MUSIC", "5000", "3000"],
+  },
+  {
+    title: "WESTERN SOLO",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["MUSIC", "5000", "3000"],
+  },
+  {
+    title: "MIME",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["THEATRE", "5000", "3000"],
+  },
+  {
+    title: "DEBATE",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["LITERARY", "3000", "1500"],
+  },
+  {
+    title: "PHTOGRAPHY",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["MEDIA", "5000", "3000"],
+  },
+  {
+    title: "SHORT FILM",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["MEDIA", "10000", "6000"],
+  },
+  {
+    title: "FACE PAINTING",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["FINE ARTS", "5000", "3000"],
+  },
+  {
+    title: "ILLUSTRATION/DIGITAL ART",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["FINE ARTS", "3000", "1500"],
+  },
+  {
+    title: "E SPORTS",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["SPORTS", "S700", "S800"],
+  },
+  {
+    title: "BEST MANAGEMENT",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["MANAGEMENT", "S900", "S200"],
+  },
+  {
+    title: "HR",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["MANAGEMENT", "S901", "S201"],
+  },
+  {
+    title: "FINANCE",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["MANAGEMENT", "S902", "S202"],
+  },
+  {
+    title: "TREASURE HUNT",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["MANAGEMENT", "S903", "S203"],
+  },
+  {
+    title: "BEST MANAGER",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["MANAGEMENT", "S904", "S204"],
+  },
+  {
+    title: "MARKETING",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["MANAGEMENT", "S906", "S206"],
+  },
+  {
+    title: "BEST ENTREPRENEUR",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["MANAGEMENT", "S909", "S209"],
+  },
+  {
+    title: "RC RACING",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["TECHNICAL", "", ""],
+  },
+  {
+    title: "LINE FOLLOW ROBOT",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["TECHNICAL", "", ""],
+  },
+];
+
+export const eventData5Star = [
+  {
+    title: "CLASSICAL GROUP DANCE",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["DANCE", "15000", "8000"],
+  },
+  {
+    title: "WESTERN GROUP DANCE",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["DANCE", "15000", "8000"],
+  },
+  {
+    title: "FOLK GROUP DANCE",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["DANCE", "15000", "8000"],
+  },
+  {
+    title: "BATTLE OF BANDS",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["MUSIC", "15000", "8000"],
+  },
+  {
+    title: "SKIT",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["THEATRE", "8000", "5000"],
+  },
+  {
+    title: "CRICKET",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["SPORTS", "S701", "S801"],
+  },
+  {
+    title: "FOOTBALL",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["SPORTS", "S702", "S802"],
+  },
+  {
+    title: "KABBADI",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["SPORTS", "S703", "S803"],
+  },
+  {
+    title: "DRONE",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["TECHNICAL", "", ""],
+  },
+];
+
+export const eventData3Star = [
+  {
+    title: "POETRY",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["LITERARY", "4000", "2000"],
+  },
+  {
+    title: "JAM",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["LITERARY", "3000", "1500"],
+  },
+  {
+    title: "CLAY MODELING",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["FINE ARTS", "3000", "1500"],
+  },
+  {
+    title: "OPERATIONS",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["MANAGEMENT", "S905", "S205"],
+  },
+  {
+    title: "AD REELS",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["MANAGEMENT", "S907", "S207"],
+  },
+  {
+    title: "MAD ADS",
+    description: "Description 1",
+    imageSrc: "images/a-l-ya631mqQ7Ng-unsplash.jpg",
+    badgeText: ["MANAGEMENT", "S908", "S208"],
+  },
+];
