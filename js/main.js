e91d0d1b8283154114acd83d2bf68b45b7ddcd0f
AmoghPadukone <35802992+AmoghPadukone@users.noreply.github.com>
2023-04-01 10:37:05 +0530

    added animate on scroll effect

--- js/main.js
+++ js/main.js
@@ -7,119 +7,124 @@ $(window).load(function () {
 function initializeSite() {
   "use strict";
 
-  //JQuery for about section
-  $(document).ready(function() {
-    // Function to check if an element is in view
-    function isInView(elem) {
-      var windowTop = $(window).scrollTop();
-      var windowBottom = windowTop + $(window).height();
-      var elemTop = $(elem).offset().top;
-      var elemBottom = elemTop + $(elem).outerHeight();
-      return ((elemBottom >= windowTop) && (elemTop <= windowBottom));
-    }
-  
-    // Function to handle the scroll event
-    function handleScroll() {
-      var section = $('.responsive-cell-block.wk-tab-12.wk-mobile-12.wk-desk-6.wk-ipadp-12.img-one');
-      var img = $('.bg-image');
-      if (isInView(section)) {
-        section.addClass('in-view');
-      } else {
-        section.removeClass('in-view');
-      }
-    }
-  
-    // Bind the scroll event to the handleScroll function
-    $(window).on('scroll', handleScroll);
-  
-    // Trigger the handleScroll function on page load
-    handleScroll();
-  });
+  // //JQuery for about section
+  // $(document).ready(function() {
+  //   // Function to check if an element is in view
+  //   function isInView(elem) {
+  //     var windowTop = $(window).scrollTop();
+  //     var windowBottom = windowTop + $(window).height();
+  //     var elemTop = $(elem).offset().top;
+  //     var elemBottom = elemTop + $(elem).outerHeight();
+  //     return ((elemBottom >= windowTop) && (elemTop <= windowBottom));
+  //   }
 
-  $(document).ready(function() {
-    // Function to check if an element is in view
-    function isInView(elem) {
-      var windowTop = $(window).scrollTop();
-      var windowBottom = windowTop + $(window).height();
-      var elemTop = $(elem).offset().top;
-      var elemBottom = elemTop + $(elem).outerHeight();
-      return ((elemBottom >= windowTop) && (elemTop <= windowBottom));
-    }
-  
-    // Function to handle the scroll event
-    function handleScroll() {
-      var section = $('.responsive-cell-block.wk-tab-12.wk-mobile-12.wk-desk-6.wk-ipadp-12.content-one');
-      
-      if (isInView(section)) {
-        section.addClass('in-view');
-      } else {
-        section.removeClass('in-view');
-      }
-    }
-  
-    // Bind the scroll event to the handleScroll function
-    $(window).on('scroll', handleScroll);
-  
-    // Trigger the handleScroll function on page load
-    handleScroll();
-  });
+  //   // Function to handle the scroll event
+  //   function handleScroll() {
+  //     var section = $('.responsive-cell-block.wk-tab-12.wk-mobile-12.wk-desk-6.wk-ipadp-12.img-one');
+  //     var img = $('.bg-image');
+  //     if (isInView(section)) {
+  //       section.addClass('in-view');
+  //     } else {
+  //       section.removeClass('in-view');
+  //     }
+  //   }
 
-  $(document).ready(function() {
-    // Function to check if an element is in view
-    function isInView(elem) {
-      var windowTop = $(window).scrollTop();
-      var windowBottom = windowTop + $(window).height();
-      var elemTop = $(elem).offset().top;
-      var elemBottom = elemTop + $(elem).outerHeight();
-      return ((elemBottom >= windowTop) && (elemTop <= windowBottom));
-    }
-  
-    // Function to handle the scroll event
-    function handleScroll() {
-      var section = $('.responsive-cell-block.wk-tab-12.wk-mobile-12.wk-desk-6.wk-ipadp-12.img-two');
-      var img = $('.bg-image2');
-      if (isInView(section)) {
-        section.addClass('in-view');
-      } else {
-        section.removeClass('in-view');
-      }
-    }
-  
-    // Bind the scroll event to the handleScroll function
-    $(window).on('scroll', handleScroll);
-  
-    // Trigger the handleScroll function on page load
-    handleScroll();
-  });
+  // Bind the scroll event to the handleScroll function
+  //   $(window).on('scroll', handleScroll);
 
-  $(document).ready(function() {
-    // Function to check if an element is in view
-    function isInView(elem) {
-      var windowTop = $(window).scrollTop();
-      var windowBottom = windowTop + $(window).height();
-      var elemTop = $(elem).offset().top;
-      var elemBottom = elemTop + $(elem).outerHeight();
-      return ((elemBottom >= windowTop) && (elemTop <= windowBottom));
-    }
-  
-    // Function to handle the scroll event
-    function handleScroll() {
-      var section = $('.responsive-cell-block.wk-tab-12.wk-mobile-12.wk-desk-6.wk-ipadp-12.content-two');
-      
-      if (isInView(section)) {
-        section.addClass('in-view');
-      } else {
-        section.removeClass('in-view');
-      }
-    }
-  
-    // Bind the scroll event to the handleScroll function
-    $(window).on('scroll', handleScroll);
-  
-    // Trigger the handleScroll function on page load
-    handleScroll();
-  });
-  
+  //   // Trigger the handleScroll function on page load
+  //   handleScroll();
+  // });
+
+  // $(document).ready(function () {
+  //   // Function to check if an element is in view
+  //   function isInView(elem) {
+  //     var windowTop = $(window).scrollTop();
+  //     var windowBottom = windowTop + $(window).height();
+  //     var elemTop = $(elem).offset().top;
+  //     var elemBottom = elemTop + $(elem).outerHeight();
+  //     return elemBottom >= windowTop && elemTop <= windowBottom;
+  //   }
+
+  //   // Function to handle the scroll event
+  //   function handleScroll() {
+  //     var section = $(
+  //       ".responsive-cell-block.wk-tab-12.wk-mobile-12.wk-desk-6.wk-ipadp-12.content-one"
+  //     );
+
+  //     if (isInView(section)) {
+  //       section.addClass("in-view");
+  //     } else {
+  //       section.removeClass("in-view");
+  //     }
+  //   }
+
+  //   // Bind the scroll event to the handleScroll function
+  //   $(window).on("scroll", handleScroll);
+
+  //   // Trigger the handleScroll function on page load
+  //   handleScroll();
+  // });
+
+  // $(document).ready(function () {
+  //   // Function to check if an element is in view
+  //   function isInView(elem) {
+  //     var windowTop = $(window).scrollTop();
+  //     var windowBottom = windowTop + $(window).height();
+  //     var elemTop = $(elem).offset().top;
+  //     var elemBottom = elemTop + $(elem).outerHeight();
+  //     return elemBottom >= windowTop && elemTop <= windowBottom;
+  //   }
+
+  //   // Function to handle the scroll event
+  //   function handleScroll() {
+  //     var section = $(
+  //       ".responsive-cell-block.wk-tab-12.wk-mobile-12.wk-desk-6.wk-ipadp-12.img-two"
+  //     );
+  //     var img = $(".bg-image2");
+  //     if (isInView(section)) {
+  //       section.addClass("in-view");
+  //     } else {
+  //       section.removeClass("in-view");
+  //     }
+  //   }
+
+  //   // Bind the scroll event to the handleScroll function
+  //   $(window).on("scroll", handleScroll);
+
+  //   // Trigger the handleScroll function on page load
+  //   handleScroll();
+  // });
+
+  // $(document).ready(function () {
+  //   // Function to check if an element is in view
+  //   function isInView(elem) {
+  //     var windowTop = $(window).scrollTop();
+  //     var windowBottom = windowTop + $(window).height();
+  //     var elemTop = $(elem).offset().top;
+  //     var elemBottom = elemTop + $(elem).outerHeight();
+  //     return elemBottom >= windowTop && elemTop <= windowBottom;
+  //   }
+
+  //   // Function to handle the scroll event
+  //   function handleScroll() {
+  //     var section = $(
+  //       ".responsive-cell-block.wk-tab-12.wk-mobile-12.wk-desk-6.wk-ipadp-12.content-two"
+  //     );
+
+  //     if (isInView(section)) {
+  //       section.addClass("in-view");
+  //     } else {
+  //       section.removeClass("in-view");
+  //     }
+  //   }
+
+  //   // Bind the scroll event to the handleScroll function
+  //   $(window).on("scroll", handleScroll);
+
+  //   // Trigger the handleScroll function on page load
+  //   handleScroll();
+  // });
 
   //OUTLINE DIMENSION AND CENTER
   (function () {
@@ -163,30 +168,27 @@ $(window).load(function () {
 });
 /* END ------------------------------------------------------- */
 
-$("#countdown").countdown({
-  date: "December 14, 2019 18:03:26",
-  render: function (data) {
-    var el = $(this.el);
-    el.empty()
-      //.append("<div>" + this.leadingZeros(data.years, 4) + "<span>years</span></div>")
-      .append(
-        "<div>" + this.leadingZeros(data.days, 2) + " <span>days</span></div>"
-      )
-      .append(
-        "<div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div>"
-      )
-      .append(
-        "<div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div>"
-      )
-      .append(
-        "<div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>"
-      );
-  },
-});
-const card = document.getElementById("cardWrap");
+// const card = document.getElementById("cardWrap");
 
-window.addEventListener("mousemove", (e) => {
-  let x = e.pageX - innerWidth / 2;
-  let y = e.pageY - innerHeight / 2;
-  card.style.transform = `translate(${x / 40}px, ${y / 40}px)`;
+// window.addEventListener("mousemove", (e) => {
+//   let x = e.pageX - innerWidth / 2;
+//   let y = e.pageY - innerHeight / 2;
+//   card.style.transform = `translate(${x / 40}px, ${y / 40}px)`;
+// });
+
+// Animate on scroll test code:
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+    // else {
+    //   entry.target.classList.remove("show");
+    // }
+  });
 });
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
