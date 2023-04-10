bef4dae6f2a42ad188878562a3c2300dbb3802b7
AmoghPadukone <amoghpadukone@gmail.com>
2023-04-10 20:33:55 +0530

    idk something

--- js/click-scroll.js
+++ js/click-scroll.js
@@ -1,7 +1,7 @@
 //jquery-click-scroll
 //by syamsul'isul' Arifin
 
-var sectionArray = [1, 2, 3, 4, 5, 6, 7];
+var sectionArray = [1, 2, 3, 4, 5, 6];
 
 $.each(sectionArray, function (index, value) {
   $(document).scroll(function () {
@@ -9,12 +9,12 @@ $.each(sectionArray, function (index, value) {
     var docScroll = $(document).scrollTop();
     var docScroll1 = docScroll + 1;
 
-    if (docScroll1 >= offsetSection) {
-      $(".navbar-nav .nav-item .nav-link").removeClass("active");
-      $(".navbar-nav .nav-item .nav-link:link").addClass("inactive");
-      $(".navbar-nav .nav-item .nav-link").eq(index).addClass("active");
-      $(".navbar-nav .nav-item .nav-link").eq(index).removeClass("inactive");
-    }
+    // if (docScroll1 >= offsetSection) {
+    //   $(".navbar-nav .nav-item .nav-link").removeClass("active");
+    //   $(".navbar-nav .nav-item .nav-link:link").addClass("inactive");
+    //   $(".navbar-nav .nav-item .nav-link").eq(index).addClass("active");
+    //   $(".navbar-nav .nav-item .nav-link").eq(index).removeClass("inactive");
+    // }
   });
 
   $(".click-scroll")
@@ -31,8 +31,8 @@ $.each(sectionArray, function (index, value) {
     });
 });
 
-$(document).ready(function () {
-  $(".navbar-nav .nav-item .nav-link:link").addClass("inactive");
-  $(".navbar-nav .nav-item .nav-link").eq(0).addClass("active");
-  $(".navbar-nav .nav-item .nav-link:link").eq(0).removeClass("inactive");
-});
+// $(document).ready(function () {
+//   $(".navbar-nav .nav-item .nav-link:link").addClass("inactive");
+//   $(".navbar-nav .nav-item .nav-link").eq(0).addClass("active");
+//   $(".navbar-nav .nav-item .nav-link:link").eq(0).removeClass("inactive");
+// });
