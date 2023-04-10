c22494bff311dc3a2da6e06d4c17af4d40cb3cca
AmoghPadukone <amoghpadukone@gmail.com>
2023-04-10 16:30:09 +0530

    file structure cleanup and schedule

--- js/main.js
+++ js/main.js
@@ -50,6 +50,27 @@ $(window).load(function () {
 });
 /* END ------------------------------------------------------- */
 
+///FAQ section JS
+
+let question = document.querySelectorAll(".questionfaq");
+
+question.forEach((question) => {
+  question.addEventListener("click", (event) => {
+    const active = document.querySelector(".questionfaq.active");
+    if (active && active !== question) {
+      active.classList.toggle("active");
+      active.nextElementSibling.style.maxHeight = 0;
+    }
+    question.classList.toggle("active");
+    const answer = question.nextElementSibling;
+    if (question.classList.contains("active")) {
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    } else {
+      answer.style.maxHeight = 0;
+    }
+  });
+});
+
 // //JQuery for about section
 // $(document).ready(function() {
 //   // Function to check if an element is in view
@@ -180,6 +201,191 @@ $(window).load(function () {
 //   let y = e.pageY - innerHeight / 2;
 //   card.style.transform = `translate(${x / 40}px, ${y / 40}px)`;
 // });
+/*---------------------------------------
+GALLERY JS
+-----------------------------------------*/
+const container = document.querySelector(".gallery-container");
+const mainSlide = document.querySelector(".main-slide");
+const sidebar = document.querySelector(".sidebar");
+const downButton = document.querySelector(".down-button");
+const upButton = document.querySelector(".up-button");
+const slidesCount = mainSlide.querySelectorAll("div").length;
+let activeSlideIndex = 0;
+
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
+
+const changeSlide = (direction) => {
+  const height = container.clientHeight;
+
+  if (direction === "up") {
+    activeSlideIndex === slidesCount - 1
+      ? (activeSlideIndex = 0)
+      : activeSlideIndex++;
+  } else if (direction === "down") {
+    activeSlideIndex < 1
+      ? (activeSlideIndex = slidesCount - 1)
+      : activeSlideIndex--;
+  }
+
+  mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
+  sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`;
+};
+
+downButton.addEventListener("click", () => {
+  changeSlide("down");
+});
+
+upButton.addEventListener("click", () => {
+  changeSlide("up");
+});
+
+document.addEventListener("keydown", (e) => {
+  console.log(e.key);
+  if (e.key === "ArrowDown") {
+    changeSlide("down");
+  } else if (e.key === "ArrowUp") {
+    changeSlide("up");
+  }
+});
+
+/*--------------------
+EVENT SLIDER JS
+--------------------*/
+
+/*--------------------
+Vars
+--------------------*/
+let progress = 10;
+let startX = 10;
+let active = 0;
+let isDown = false;
+let windowWidth = window.innerWidth;
+var WIDTH_LIMIT = 820;
+/*--------------------
+Contants
+--------------------*/
+const speedWheel = 0.02;
+const speedDrag = -0.1;
+
+/*--------------------
+Get Z
+--------------------*/
+const getZindex = (array, index) =>
+  array.map((_, i) =>
+    index === i ? array.length : array.length - Math.abs(index - i)
+  );
+
+/*--------------------
+Items
+--------------------*/
+const $items = document.querySelectorAll(".epic-carousel-item");
+const $cursors = document.querySelectorAll(".cursor");
+
+const displayItems = (item, index, active) => {
+  const zIndex = getZindex([...$items], active)[index];
+  item.style.setProperty("--zIndex", zIndex);
+  item.style.setProperty("--active", (index - active) / $items.length);
+};
+
+/*--------------------
+Control Buttons
+--------------------*/
+const prevBtn = document.getElementById("slider-previous");
+const nextBtn = document.getElementById("slider-next");
+
+/*--------------------
+Animate
+--------------------*/
+const animate = () => {
+  progress = Math.max(0, Math.min(progress, 100));
+  active = Math.floor((progress / 100) * ($items.length - 1));
+
+  $items.forEach((item, index) => displayItems(item, index, active));
+};
+animate();
+
+/*--------------------
+Click on Items
+--------------------*/
+$items.forEach((item, i) => {
+  item.addEventListener("click", () => {
+    progress = (i / $items.length) * 100 + 10;
+    animate();
+    // console.log("progress=" + progress);
+  });
+});
+
+/*--------------------
+Control buttanssss 😎
+--------------------*/
+
+prevBtn.addEventListener("click", (e) => {
+  progress = progress - 10;
+  animate();
+  if (progress <= 0) {
+    progress = 10;
+  }
+
+  console.log("progress=" + progress);
+  console.log(windowWidth);
+});
+nextBtn.addEventListener("click", (e) => {
+  progress = progress + 10;
+  animate();
+  console.log("progress=" + progress);
+});
+
+/*--------------------
+Handlers
+--------------------*/
+const handleWheel = (e) => {
+  if (windowWidth <= WIDTH_LIMIT) {
+    const wheelProgress = e.deltaY * speedWheel;
+    progress = progress + wheelProgress;
+    animate();
+  }
+};
+
+const handleMouseMove = (e) => {
+  if (windowWidth <= WIDTH_LIMIT) {
+    if (e.type === "mousemove") {
+      $cursors.forEach(($cursor) => {
+        $cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+      });
+    }
+    if (!isDown) return;
+    const x = e.clientX || (e.touches && e.touches[0].clientX) || 0;
+    const mouseProgress = (x - startX) * speedDrag;
+    progress = progress + mouseProgress;
+    startX = x;
+    animate();
+  }
+};
+
+const handleMouseDown = (e) => {
+  if (windowWidth <= WIDTH_LIMIT) {
+    isDown = true;
+    startX = e.clientX || (e.touches && e.touches[0].clientX) || 0;
+  }
+};
+
+const handleMouseUp = () => {
+  if (windowWidth <= WIDTH_LIMIT) {
+    isDown = false;
+  }
+};
+
+/*--------------------
+Listeners
+--------------------*/
+document.addEventListener("mousewheel", handleWheel);
+document.addEventListener("mousedown", handleMouseDown);
+document.addEventListener("mousemove", handleMouseMove);
+document.addEventListener("mouseup", handleMouseUp);
+document.addEventListener("touchstart", handleMouseDown);
+document.addEventListener("touchmove", handleMouseMove);
+document.addEventListener("touchend", handleMouseUp);
+/*----------------------------------------------------------------------------------*/
 
 // Animate on scroll JS:
 const observer = new IntersectionObserver((entries) => {
