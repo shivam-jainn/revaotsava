dae56c54c072969ae5df7b9531375a6a455afac3
Sai-Prasad-C <r20200886saiprasadc@cse.reva.edu.in>
2023-04-11 20:20:41 +0530

    Nav bar onscroll animation

--- js/main.js
+++ js/main.js
@@ -50,6 +50,31 @@ $(window).load(function () {
 });
 /* END ------------------------------------------------------- */
 
+//Nav bar
+
+el_autohide = document.querySelector('.autohide');
+  
+  // add padding-top to bady (if necessary)
+  navbar_height = document.querySelector('.navbar').offsetHeight;
+  document.body.style.paddingTop = navbar_height + 'px';
+
+  if(el_autohide){
+    var last_scroll_top = 0;
+    window.addEventListener('scroll', function() {
+          let scroll_top = window.scrollY;
+         if(scroll_top < last_scroll_top) {
+              el_autohide.classList.remove('scrolled-down');
+              el_autohide.classList.add('scrolled-up');
+          }
+          else {
+              el_autohide.classList.remove('scrolled-up');
+              el_autohide.classList.add('scrolled-down');
+          }
+          last_scroll_top = scroll_top;
+    }); 
+    // window.addEventListener
+  }
+
 ///FAQ section JS
 
 let question = document.querySelectorAll(".questionfaq");
